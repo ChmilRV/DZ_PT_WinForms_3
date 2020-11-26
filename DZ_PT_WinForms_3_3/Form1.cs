91a2437d67e0b6c4004b4563aa04672047cd936f
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-11-26 21:06:33 +0200

    2106

--- DZ_PT_WinForms_3_3/Form1.cs
+++ DZ_PT_WinForms_3_3/Form1.cs
@@ -10,14 +10,7 @@ namespace DZ_PT_WinForms_3_3
         public Form1()
         {
             InitializeComponent();
-
             timer1.Start();
-        }
-
-        public string TText
-        {
-            set { textBox_textOpen.Text = value; }
-            //get { return textBox_textEdit.Text; }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -74,22 +67,17 @@ namespace DZ_PT_WinForms_3_3
                 MessageBox.Show("Файл сохранен.");
             }
         }
-
+        
         private void button_edit_Click(object sender, EventArgs e)
         {
+            //___________________________Не понял как вернуть из немодальной формы измененный текст, поэтому форма модальная...
             Form2 form2 = new Form2(this, textBox_textOpen.Text);
-            form2.Show();
-            isChanged = true;
-            SaveFile_ToolStripMenuItem.Enabled = true;
-
-            //if (form2.ShowDialog() == DialogResult.OK)
-            //{
-            //    textBox_textOpen.Text=(form2.TText);
-            //    isChanged = true;
-            //    SaveFile_ToolStripMenuItem.Enabled = true;
-            //}
-
-
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                textBox_textOpen.Text = (form2.TText);
+                isChanged = true;
+                SaveFile_ToolStripMenuItem.Enabled = true;
+            }
         }
 
         private void SaveFile_ToolStripMenuItem_Click(object sender, EventArgs e)
--- DZ_PT_WinForms_3_3/Form2.cs
+++ DZ_PT_WinForms_3_3/Form2.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Drawing.Printing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DZ_PT_WinForms_3_3
@@ -21,10 +13,10 @@ namespace DZ_PT_WinForms_3_3
             timer1.Start();
         }
 
-        //public string TText
-        //{
-        //    get { return textBox_textEdit.Text; }
-        //}
+        public string TText
+        {
+            get { return textBox_textEdit.Text; }
+        }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -41,56 +33,32 @@ namespace DZ_PT_WinForms_3_3
         {
             MessageBox.Show("Простейший текстовый редактор.\nchmilrv@gmail.com", "О программе", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-       
-
-
-
 
         private void button_save_Click(object sender, EventArgs e)
         {
-            textBox_textEdit.Text=form1.TText;
-            
-            
-
-
-            //this.DialogResult = DialogResult.OK;
-        }
-
-
-        private void Save_ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-
-
             this.DialogResult = DialogResult.OK;
         }
 
+        private void Save_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
 
         private void button_cancel_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Cancel;
         }
+
         private void Cancel_ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Cancel;
         }
 
-
-        
-
-
-
-        
-
-
-
         private void button_font_Click(object sender, EventArgs e)
         {
             FontDialog fd = new FontDialog();
             if (fd.ShowDialog() == DialogResult.OK)
-            {
                 textBox_textEdit.Font = fd.Font;
-            }
         }
 
         internal PrintPreviewDialog PrintPreviewDialog1;
@@ -98,74 +66,34 @@ namespace DZ_PT_WinForms_3_3
 
         private void InitializePrintPreviewDialog()
         {
-            // Create a new PrintPreviewDialog using constructor.
-            this.PrintPreviewDialog1 = new PrintPreviewDialog();
-
-            //Set the size, location, and name.
-            this.PrintPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
-            this.PrintPreviewDialog1.Location = new System.Drawing.Point(29, 29);
-            this.PrintPreviewDialog1.Name = "PrintPreviewDialog1";
-
-            // Associate the event-handling method with the 
-            // document's PrintPage event.
-            this.document.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(document_PrintPage);
-
-            // Set the minimum size the dialog can be resized to.
-            this.PrintPreviewDialog1.MinimumSize = new System.Drawing.Size(375, 250);
-
-            // Set the UseAntiAlias property to true, which will allow the 
-            // operating system to smooth fonts.
-            this.PrintPreviewDialog1.UseAntiAlias = true;
+            PrintPreviewDialog1 = new PrintPreviewDialog();
+            PrintPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            PrintPreviewDialog1.Location = new System.Drawing.Point(29, 29);
+            PrintPreviewDialog1.Name = "PrintPreviewDialog1";
+            document.PrintPage += new PrintPageEventHandler(document_PrintPage);
+            PrintPreviewDialog1.MinimumSize = new System.Drawing.Size(375, 250);
+            PrintPreviewDialog1.UseAntiAlias = true;
         }
 
-        private void document_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        private void document_PrintPage(object sender, PrintPageEventArgs e)
         {
-            // Insert code to render the page here.
-            // This code will be called when the PrintPreviewDialog.Show 
-            // method is called.
-
             e.Graphics.DrawString(textBox_textEdit.Text, textBox_textEdit.Font, System.Drawing.Brushes.Black, 20, 20);
         }
 
         private void button_PrintPreviewDialog_Click(object sender, EventArgs e)
         {
             InitializePrintPreviewDialog();
-
-            document.DocumentName = "Text";
-
-            // Set the PrintPreviewDialog.Document property to
-            // the PrintDocument object selected by the user.
             PrintPreviewDialog1.Document = document;
-
-            // Call the ShowDialog method. This will trigger the document's
-            //  PrintPage event.
             PrintPreviewDialog1.ShowDialog();
-
-
-
         }
 
         private void button_PageSetupDialog_Click(object sender, EventArgs e)
         {
             PageSetupDialog pageSetupDialog = new PageSetupDialog();
-            // Initialize the dialog's PrinterSettings property to hold user
-            // defined printer settings.
-            pageSetupDialog.PageSettings = new System.Drawing.Printing.PageSettings();
-
-            // Initialize dialog's PrinterSettings property to hold user
-            // set printer settings.
-            pageSetupDialog.PrinterSettings = new System.Drawing.Printing.PrinterSettings();
-
-            //Do not show the network in the printer dialog.
+            pageSetupDialog.PageSettings = new PageSettings();
+            pageSetupDialog.PrinterSettings = new PrinterSettings();
             pageSetupDialog.ShowNetwork = false;
-
-            //Show the dialog storing the result.
-            DialogResult result = pageSetupDialog.ShowDialog();
-
-            // If the result is OK, display selected settings in
-            // ListBox1. These values can be used when printing the
-            // document.
-            if (result == DialogResult.OK)
+            if (pageSetupDialog.ShowDialog() == DialogResult.OK)
             {
                 string[] results = new string[]
                 {
